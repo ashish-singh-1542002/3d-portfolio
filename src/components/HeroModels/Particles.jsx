58c6dc1c07157bec680b731fe2408310babe14de
ashish-singh-1542002 <122450260+ashish-singh-1542002@users.noreply.github.com>
2026-05-27 08:34:31 +0530

    particles

--- src/components/HeroModels/Particles.jsx
+++ src/components/HeroModels/Particles.jsx
@@ -1,7 +1,7 @@
 import { useRef, useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
 
-const Particles = ({ count = 1000 }) => {
+const Particles = ({ count = 2000 }) => {
   const mesh = useRef();
 
   const particles = useMemo(() => {
